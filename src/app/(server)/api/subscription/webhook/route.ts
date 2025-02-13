ba9861c30e826a22debc3de401f21b026caf9b42
Gabriel Ribeiro <grxgabriel@gmail.com>
2025-02-13 18:38:19 -0300

    Add new event listener to webhook and add trailing slash

--- src/app/(server)/api/subscription/webhook/route.ts
+++ src/app/(server)/api/subscription/webhook/route.ts
@@ -33,11 +33,11 @@ export async function POST(req: Request) {
 
     const event = stripeClient.webhooks.constructEvent(requestText, signature, webhookSecret)
 
-    if (event.type === 'checkout.session.completed') {
+    if (event.type === 'checkout.session.completed' || event.type === 'checkout.session.async_payment_succeeded') {
       return checkoutSessionUpdate(event.data.object, 'success')
     }
 
-    if (event.type === 'checkout.session.async_payment_failed') {
+    if (event.type === 'checkout.session.expired' || event.type === 'checkout.session.async_payment_failed') {
       return checkoutSessionUpdate(event.data.object, 'failure')
     }
 
