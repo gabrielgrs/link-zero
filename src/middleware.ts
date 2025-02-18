0ea99e24ebd2ed72a87166bf1f5a2daee3529f7f
Gabriel Ribeiro <grxgabriel@gmail.com>
2025-02-18 15:16:33 -0300

    Inject token via middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { cookiesConfigs } from './utils/cookies/configs'
 
 export async function middleware(request: NextRequest) {
   const { origin } = request.nextUrl
   const redirectTo = request.nextUrl.searchParams.get('redirectTo')
+  const token = request.nextUrl.searchParams.get('token')
 
   if (request.url.includes('/logout')) {
     const response = NextResponse.redirect(new URL(redirectTo || '/', origin))
 
     response.cookies.delete('token')
+    return response
+  }
+
+  if (request.url.includes('/auth') && token) {
+    const response = NextResponse.redirect(new URL(redirectTo || '/dashboard', origin))
+    response.cookies.set('token', token, cookiesConfigs)
     return response
   }
 }
